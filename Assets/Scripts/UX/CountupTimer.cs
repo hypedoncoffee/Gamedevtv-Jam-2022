10865365b4cf5de9dea70380f3b394c7ea4168af
Hypedoncoffee <hypedoncoffee@gmail.com>
2022-05-28 20:36:56 -0400

    Countup

---
+++ Assets/Scripts/UX/CountupTimer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountupTimer : MonoBehaviour
+{
+    private float timeSinceStart;
+    TextMeshProUGUI timerText;
+    [SerializeField] bool countdown;
+    [SerializeField] int maxTime;
+    float timeRemaining;
+    void Awake()
+    {
+        if(countdown)
+            timeRemaining = maxTime;
+        timeSinceStart = 0;
+        timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        if(!countdown)
+        timerText.text = String.Format("{0:0} seconds", timeSinceStart);
+
+        else 
+        {
+            timeRemaining -=Time.deltaTime;
+            timerText.text = String.Format("{0:0} until blackout ends", timeRemaining);
+        }
+    }
+}
